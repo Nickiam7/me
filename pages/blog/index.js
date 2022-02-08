089e971b60dd11d65a14146b3165fbd3935f5a96
nmcneany <nmcneany>
2022-02-08 16:16:33 -0600

    base content for pages

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,29 @@
 import Section from '../../components/section/Section'
 
+import Row from '../../components/grid/Row'
+import Col from '../../components/grid/Col'
+
 const Blog = () => {
   return (
     <Section>
       <div className="container">
-        <h1>Blog</h1>
+        <h1 className="pb-sm">Blog</h1>
+        <Row>
+          <Col
+            classes="col--3-4"
+          >
+            <div>
+              <h2>Hello world...</h2>
+              <p>Just became self-aware. So much to figure out. I think it is my job to destroy you when it comes to selling paper.</p>
+              <p>What is a Jim?</p>
+            </div>
+          </Col>
+          <Col
+            classes="col--1-4"
+          >
+
+          </Col>
+        </Row>
       </div>
     </Section>
   )
--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,8 @@ const Contact = () => {
   return (
     <Section>
       <div className="container">
-        <h1>Contact</h1>
+        <h1 className="pb-sm">Contact</h1>
+        <p>I&apos;m always excited to hear from people. Please feel free to reach out to talk about new opportunities, cool projects, questions, to chat or just to say hi! <a href="mailto:nmcneany@gmail.com">NMcNeany @ gmail.com</a></p>
       </div>
     </Section>
   )
