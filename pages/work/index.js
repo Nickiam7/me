28bdf318f997d3a91f861ee899c06093de80edd7
nmcneany <nmcneany>
2022-02-08 16:20:32 -0600

    fix noreferrer error

--- pages/work/index.js
+++ pages/work/index.js
@@ -5,7 +5,7 @@ const Work = () => {
     <Section>
       <div className="container">
         <h1 className="pb-sm">Work</h1>
-        <p>I&apos;ll have a better organized page to show case recent work samples soon, but for now you can check out my <a href="https://github.com/Nickiam7" target="_blank" rel="noopener" rel="noreferrer">GitHub page</a>.</p>
+        <p>I&apos;ll have a better organized page to show case recent work samples soon, but for now you can check out my <a href="https://github.com/Nickiam7" target="_blank" rel="noreferrer">GitHub page</a>.</p>
       </div>
     </Section>
   )
